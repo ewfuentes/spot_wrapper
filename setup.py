e57267daa4e57a02b88f33bb18c130467b4e0f24
Erick Fuentes <erick@csail.mit.edu>
2024-02-19 16:29:26 -0500

    pin python libraries

--- setup.py
+++ setup.py
@@ -5,5 +5,13 @@ setup(
     version="1.0.0",
     description="Wrapper for Boston Dynamics Spot SDK",
     packages=find_packages(include=["spot_wrapper*"]),
-    install_requires=["bosdyn-client", "bosdyn-api", "bosdyn-mission", "bosdyn-core", "grpcio", "inflection", "pytest"],
+    install_requires=[
+        "bosdyn-client==3.3.2",
+        "bosdyn-api==3.3.2",
+        "bosdyn-mission==3.3.2",
+        "bosdyn-core==3.3.2",
+        "grpcio",
+        "inflection",
+        "pytest"
+    ],
 )
